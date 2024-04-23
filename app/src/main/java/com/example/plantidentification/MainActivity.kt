49adde8c4a9736a4b7aec209c05f8961d94e49de
Ajay Teli <210339616286@paruluniversity.ac.in>
2024-04-23 17:00:05 +0530

    Initial commit

---
+++ app/src/main/java/com/example/plantidentification/MainActivity.kt
@@ -0,0 +1,127 @@
+package com.example.plantidentification
+
+
+import android.os.Bundle
+import android.widget.Toast
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.ActivityInfo
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
+import android.os.Build
+import android.provider.MediaStore
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Gravity
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.example.plantidentification.databinding.ActivityMainBinding
+import isomora.com.greendoctor.Classifier
+import java.io.IOException
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
+    private lateinit var mClassifier: Classifier
+    private lateinit var mBitmap: Bitmap
+
+    private val mCameraRequestCode = 0
+    private val mGalleryRequestCode = 2
+
+    private val mInputSize = 224
+    private val mModelPath = "plant_disease_model.tflite"
+    private val mLabelPath = "plant_labels.txt"
+    private val mSamplePath = "soybean.JPG"
+
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding=DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
+
+        mClassifier = Classifier(assets, mModelPath, mLabelPath, mInputSize)
+
+        resources.assets.open(mSamplePath).use {
+            mBitmap = BitmapFactory.decodeStream(it)
+            mBitmap = Bitmap.createScaledBitmap(mBitmap, mInputSize, mInputSize, true)
+            binding.mPhotoImageView.setImageBitmap(mBitmap)
+        }
+
+       binding.mCameraButton.setOnClickListener {
+            val callCameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(callCameraIntent, mCameraRequestCode)
+        }
+
+        binding.mGalleryButton.setOnClickListener {
+            val callGalleryIntent = Intent(Intent.ACTION_PICK)
+            callGalleryIntent.type = "image/*"
+            startActivityForResult(callGalleryIntent, mGalleryRequestCode)
+        }
+        binding.mDetectButton.setOnClickListener {
+            val results = mClassifier.recognizeImage(mBitmap).firstOrNull()
+           binding.mResultTextView.text= results?.title+"\n Confidence:"+results?.confidence
+
+        }
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == mCameraRequestCode){
+            //Considers le cas de la caméra annulée
+            if(resultCode == Activity.RESULT_OK && data != null) {
+                mBitmap = data.extras!!.get("data") as Bitmap
+                mBitmap = scaleImage(mBitmap)
+                val toast = Toast.makeText(this, ("Image crop to: w= ${mBitmap.width} h= ${mBitmap.height}"), Toast.LENGTH_LONG)
+                toast.setGravity(Gravity.BOTTOM, 0, 20)
+                toast.show()
+                binding.mPhotoImageView.setImageBitmap(mBitmap)
+                binding.mResultTextView.text= "Your photo image set now."
+            } else {
+                Toast.makeText(this, "Camera cancel..", Toast.LENGTH_LONG).show()
+            }
+        } else if(requestCode == mGalleryRequestCode) {
+            if (data != null) {
+                val uri = data.data
+
+                try {
+                    mBitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, uri)
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+
+                println("Success!!!")
+                mBitmap = scaleImage(mBitmap)
+                binding.mPhotoImageView.setImageBitmap(mBitmap)
+
+            }
+        } else {
+            Toast.makeText(this, "Unrecognized request code", Toast.LENGTH_LONG).show()
+
+        }
+    }
+
+
+    fun scaleImage(bitmap: Bitmap?): Bitmap {
+        val orignalWidth = bitmap!!.width
+        val originalHeight = bitmap.height
+        val scaleWidth = mInputSize.toFloat() / orignalWidth
+        val scaleHeight = mInputSize.toFloat() / originalHeight
+        val matrix = Matrix()
+        matrix.postScale(scaleWidth, scaleHeight)
+        return Bitmap.createBitmap(bitmap, 0, 0, orignalWidth, originalHeight, matrix, true)
+    }
+
+}
